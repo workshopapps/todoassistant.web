e4a01734bb661b33a8005331d77983ab3ba7230f
Kingsley Solomon <kinxly@gmail.com>
2022-11-17 08:07:13 +0100

    feat & style: landing-page section-two completed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/landing-page/hero-section/Hero";
+import SectionTwo from "../components/landing-page/section-two/SectionTwo";
 
 const LandingPage = () => {
   return (
     <main>
       <Hero />
+      <SectionTwo />
     </main>
   );
 };
