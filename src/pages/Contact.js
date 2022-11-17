1ab0873fb52cbc1b5b3e64d79e59cdee99a3f3c5
Tolulope-xo <olatunjitolulope4@gmail.com>
2022-11-17 04:30:30 +0100

    created the map component on the Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import Map from '../Components/Contact_map/Map';
 const Contact = () => {
-  return <div>Contact</div>;
+  return <div>
+    <Map/>
+  </div>;
 };
 
 export default Contact;
