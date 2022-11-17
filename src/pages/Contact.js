4f61b9369048d75b45b8c2c561349b0cf38e89ef
Tolulope-xo <olatunjitolulope4@gmail.com>
2022-11-17 12:27:55 +0100

    feat: removed the components in the pages folder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Map from "../Components/Contact_map/Map";
-import Form from "../Components/Contact_form/Form";
 const Contact = () => (
   <div>
-    <Form/>
-    <Map />;
+    
   </div>
 );
 
