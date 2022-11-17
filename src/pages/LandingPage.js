26aa1ebeec69b2ba644e005ec061e5052143fe42
PeroHub <65111015+PeroHub@users.noreply.github.com>
2022-11-17 04:26:03 -0800

    clean up in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import Client from '../layout/client/Client'
-import Download from '../layout/download/Download'
-import Gift from '../layout/gift/Gift'
+// import Client from '../layout/client/Client'
+// import Download from '../layout/download/Download'
+// import Gift from '../layout/gift/Gift'
 
 const LandingPage = () => {
   return (
-    <div>
-      <Gift />
-      <Client />
-      <Download />
-    </div>
+    <div>Landing page</div>
   )
 }
 
