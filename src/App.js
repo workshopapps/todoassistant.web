c43cd18d9c161570d2ca7a81e7f0045699526a58
[damisi00] <enioluwafeo@gmail.com>
2022-11-17 17:33:30 +0100

    feature: VA modal screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
+// import VaModal from "./Components/VA-modal/VaModal";
 import LandingPage from "./pages/LandingPage";
 import Contact from "./pages/Contact";
 import Career from "./pages/Career";
@@ -15,21 +16,23 @@ import Premium from "./layout/FAQ/Premium/Premium";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/career" element={<Career />} />
-        <Route path="/faq" element={<FAQ />}>
-          <Route index element={<Account />} />
-          <Route path="account" element={<Account />} />
-          <Route path="tasks" element={<Task />} />
-          <Route path="premium" element={<Premium />} />
-        </Route>
-        <Route path='/about-us' element={<AboutUs/>} />
-        <Route path="/policy" element={<PrivatePolicy />} />
-      </Routes>
-      <Footer />
+       <Header />
+       <Routes>
+         <Route path="/" element={<LandingPage />} />
+         <Route path="/contact" element={<Contact />} />
+         <Route path="/career" element={<Career />} />
+         <Route path="/faq" element={<FAQ />}>
+           <Route index element={<Account />} />
+           <Route path="account" element={<Account />} />
+           <Route path="tasks" element={<Task />} />
+           <Route path="premium" element={<Premium />} />
+         </Route>
+         <Route path='/about-us' element={<AboutUs/>} />
+         <Route path="/policy" element={<PrivatePolicy />} />
+       </Routes>
+       <Footer />
+       {/* <Route path="/va-modal" element={<VaModal />} /> */}
+      
     </div>
   );
 }
