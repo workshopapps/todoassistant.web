b92fb1f512cf1b4d1a72ea9b163cdec26d64a1a5
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-11-17 12:12:08 +0100

    fix:remove the import of the careerMiddle component

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,13 +1,10 @@
 import React from "react";
-import CareerMiddle from "../layout/CareerMiddle";
 import styles from "./Career.module.scss";
 
 const Career = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <CareerMiddle />
-      </div>
+      <div className={styles.container}></div>
     </div>
   );
 };
