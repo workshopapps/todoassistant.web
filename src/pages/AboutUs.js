6d888d9ab3aa3eafff13e555049d6ade5b9798b5
Ijeoma Uwabunkeonye <uwabunkeonyeijeoma@gmail.com>
2022-11-17 11:41:26 +0100

    testimonal

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AboutUsHeader from '../components/AboutUs/AboutUsHeader';
-
+import Testimonal from '../components/AboutUs/Testimonal';
 const AboutUs = () => {
   return (
     <>
       <AboutUsHeader/>
+      <Testimonal/>
     </>
   );
 };
