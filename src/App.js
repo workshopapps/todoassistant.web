3636874158b2404225aefc3a428cdf7865618de0
Adesina Adebisi <adeboysina@gmail.com>
2022-11-17 01:10:02 +0100

    FAQ Page top added

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import FAQTop from './components/FAQTop';
 
 function App() {
   return (
-    <>
-    <FAQTop/>
-    </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
---
+++ src/pages/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FAQTop from "../components/FAQTop"
+
+const FAQ = () => {
+  return (
+    <div>
+        <FAQTop/>
+    </div>
+  )
+}
+
+export default FAQ
