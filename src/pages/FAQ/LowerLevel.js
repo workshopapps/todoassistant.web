3b3e77b6ca6efe6f44388df61e221faf34218485
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-11-18 21:35:03 +0100

    FAQs page

--- src/pages/FAQ/LowerLevel.js
+++ src/pages/FAQ/LowerLevel.js
@@ -1,39 +1,75 @@
-import { NavLink } from "react-router-dom";
-import { subRoute } from "./static";
-import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { QandA } from "./static";
+import { contact } from "./static";
+import plus from "../../assets/add.svg"
+import minus from "../../assets/minus.svg"
+import { useState } from "react";
+import { Link } from 'react-router-dom';
 import "./FAQ.scss";
 
+
 const LowerLevel = () => {
+
+  const [active, setActive] = useState(-1);
+
+  const Toggle = (index) => {
+    if (index === active) {
+      setActive(-1);
+      return;
+    }
+    setActive(index);
+  };
+
   return (
-    <div className="Topp ">
-      <div className="faq container ">
-        {subRoute.map((route) => {
-          return (
-            <NavLink
-              to={route.location}
-              key={route.id}
-              className={({ isActive }) => {
-                return (
-                  "links" + (isActive ? "links activee" : "links inactivee")
-                );
-              }}
-            >
-              {route.heading}
-              <div className="line"></div>
-            </NavLink>
-          );
-        })}
-      </div>
-      <Outlet />
-      <div className="text container ">
-        <p className="contact">
-          Cannot find the answer you’re looking for? Please chat with our
-          friendly team.
-        </p>
-        <Link to="/contact">
-          <p className="contact-sub">Contact Us</p>
-        </Link>
+    <div>
+      <header className="top">
+        <div className="ls container_2">
+          <h2 className="header-text">
+            {
+              contact.header
+            }
+          </h2>
+          <span className="sub-text">
+            {
+              contact.sub_text
+            }
+          </span>
+        </div>
+      </header>
+      <div className="Topp ">
+        <div className="faq container ">
+          {
+            QandA.map((qA, key) => {
+              return <div key={key} className="mapped" onClick={() => Toggle(key)}>
+                <div className="question-toggle">
+                  <div className="question">{qA.question}</div>
+                  {active === key ? (
+                    <img src={minus} alt="" className="icon" />
+                  ) : (
+                    <img src={plus} alt="" className="icon" />
+                  )}
+                </div>
+                <span className={`sub  ${active === key ? "active" : "inactive"}`}>
+                  {qA.answer}
+                </span>
+              </div>
+            })
+          }
+
+        </div>
+        <div className="text container ">
+          <p className="contact">
+            {
+              contact.contact
+            }
+          </p>
+          <Link to="/contact">
+            <p className="contact-sub">
+              {
+                contact.link
+              }
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
