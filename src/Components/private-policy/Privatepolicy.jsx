961b88f3d137a197bc0ecec871b43446625bc9ff
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-18 07:10:59 +0100

    update and style web private policy page content

--- src/Components/private-policy/Privatepolicy.jsx
+++ src/Components/private-policy/Privatepolicy.jsx
@@ -14,59 +14,87 @@ function Privatepolicy() {
   return (
     <div className="privatepolicy">
         <div className="pagecontainer">
-            <h1>Privacy policy</h1>
-            <p>We know that you care how information about you is used and shared, and we appreciate your trust that we will do so carefully and sensibly. This Privacy Notice describes how Ticked collects and processes your personal information.</p>
-            <div className="personal_data">
-              <h2>Personal data we collected</h2>
-              <p>Ticked obtains information about you through our contacts
-                with you. Some of this data is provided directly by you,
-                and some is obtained through data collection about your
-                interactions, use, and experiences with our product. The
-                data we gather is determined by the context of your
-                interactions with Ticked as well as the choices you make,
-                such as your privacy settings and the products and features
-                you use. Third-party data about you is also obtained by us.
-                When it comes to the technology you use and the data you
-                disclose, you have options. When we ask for personal
-                information, you have the option to decline. To offer you
-                with a service, our product may require some personal
-                information. You will not be able to use a product or
-                service if you do not supply the necessary info. Similarly,
-                if we are required by law or to enter into or carry out a
-                contract with you and you fail to provide the data, we will
-                be unable to enter into the contract; or, if this relates
-                to an existing product you are using, we may be forced to
-                suspend or cancel it. If this is the case at the time, we
-                will notify you. When supplying the data is voluntary,
-                and you choose not to provide personal information,
-                functions like personalization that rely on such information
-                will not function for you. <a href='#'>Learn More</a></p>
+            <div className="privacy_header">
+                <h1>Privacy policy</h1>
+                <p>
+                Your privacy is important to us. It is TICKED APP's policy to respect 
+                your privacy regarding any information we may collect from you across 
+                our website and TICKED APP. We only ask for personal information when 
+                we truly need it to provide a service to you. We collect it by fair and 
+                lawful means, with your knowledge and consent. We also let you know why 
+                we’re collecting it and how it will be used. We only retain collected 
+                information for as long as necessary to provide you with your requested 
+                service. What data we store, we’ll protect within commercially acceptable 
+                means to prevent loss and theft, as well as unauthorised access, disclosure, 
+                copying, use or modification. We don’t share any personally identifying 
+                information publicly or with third-parties, except when required to by law. 
+                Our website may link to external sites that are not operated by us. Please 
+                be aware that we have no control over the content and practices of these sites, 
+                and cannot accept responsibility or liability for their respective privacy 
+                policies. You are free to refuse our request for your personal information, 
+                with the understanding that we may be unable to provide you with some of your 
+                desired services. Your continued use of our website will be regarded as 
+                acceptance of our practices around privacy and personal information. If you 
+                have any questions about how we handle user data and personal information, 
+                feel free to contact us: mailto:<a href="#">Teamrulerhng@gmail.com</a>
+                </p>
             </div>
-            <div className="how_we_use_data">
-              <h2>How we use your data</h2>
-              <p>Ticked uses the data we collect to provide you with rich,
-                interactive experiences. In particular, we use data to:
-              </p>
-              <p>
-                <ul>
-                  <li>Improve and develop our products.</li>
-                  <li>Personalize our products and make recommendations.</li>
-                  <li>Bring to you Pro Versions.</li>
-                </ul>
-              </p>
-              <p>
-                We also use data to run our business, which includes analyzing
-                performance, meeting regulatory requirements, growing our personnel,
-                and doing research. <a href='#'>Learn More</a>
-              </p>
+
+            <div className="blockparagraph">
+                <h2>Cookie Policy for TICKED APP</h2>
+                <p>
+                    This is the Cookie Policy for TICKED APP. This site uses cookies, which are 
+                    tiny files that are downloaded to your computer, to improve your experience. 
+                    This page describes what information they gather, how we use it and why we 
+                    sometimes need to store these cookies. We will also share how you can prevent 
+                    these cookies from being stored. However, this may downgrade or 'break' 
+                    certain elements of the sites functionality. <br />
+                    How We Use Cookies? We use cookies for a variety of reasons detailed below. 
+                    Unfortunately in most cases there are no industry standard options for disabling 
+                    cookies without completely disabling the functionality and features they add to 
+                    this site. It is recommended that you leave on all cookies if you are not sure 
+                    whether you need them or not in case they are used to provide a service that you 
+                    use. Disabling Cookies You can prevent the setting of cookies by adjusting the 
+                    settings on your browser (see your browser Help for how to do this). Be aware 
+                    that disabling cookies will affect the functionality of this and many other 
+                    websites that you visit. Disabling cookies will usually result in also disabling 
+                    certain functionality and features of the this site. Therefore it is recommended 
+                    that you do not disable cookies. The Cookies We Set Account related cookies If you
+                    create an account with us then we will use cookies for the management of the signup 
+                    process and general administration. These cookies will usually be deleted when you 
+                    log out however in some cases they may remain afterwards to remember your site 
+                    preferences when logged out.
+                </p>
             </div>
-            <div className="acceptance">
-              <h2>Acceptance and Concent</h2>
-              <p>
-                Ticked 1`offers you an avenue to read the terms and conditions before
-                acceptance happens. With ticked human rights are respected as we make sure
-                that the data used has been consented by you the user.
-              </p>
+            <div className="blockparagraph">
+                <h2>Login related cookies</h2>
+                <p>
+                    We use cookies when you are logged in so that we can remember this fact. This prevents you from having to log in every single time you visit a new page. These cookies are typically removed or cleared when you log out to ensure that you can only access restricted features and areas when logged in.
+                </p>
+            </div>
+            <div className="blockparagraph">
+                <h2>Email newsletters related cookies</h2>
+                <p>
+                    This site offers newsletter or email subscription services and cookies may be used to remember if you are already registered and whether to show certain notifications which might only be valid to subscribed/unsubscribed users. Orders processing related cookies This site offers e-commerce or payment facilities and some cookies are essential to ensure that your order is remembered between pages so that we can process it properly. Surveys related cookies From time to time we offer user surveys and questionnaires to provide you with interesting insights, helpful tools, or to understand our user base more accurately. These surveys may use cookies to remember who has already taken part in a survey or to provide you with accurate results after you change pages. Forms related cookies When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.
+                </p>
+            </div>
+            <div className="blockparagraph">
+                <h2>Site preferences cookies</h2>
+                <p>
+                    In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.
+                </p>
+            </div>
+            <div className="blockparagraph">
+                <h2>Third Party Cookies</h2>
+                <p>
+                    In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site. <br/>
+                    This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content. For more information on Google Analytics cookies, see the official Google Analytics page. <br/>
+                    Third party analytics are used to track and measure usage of this site so that we can continue to produce engaging content. These cookies may track things such as how long you spend on the site or pages you visit which helps us to understand how we can improve the site for you. From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most. As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price. <br />
+                    We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. We set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies. <br />
+                    More Information. <br />
+                    Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. This policy is effective as of Nov 2022. <br />
+                    ReplyForward
+                </p>
             </div>
             <div className="back_to_top">
               <a onClick={scrollTop}>
