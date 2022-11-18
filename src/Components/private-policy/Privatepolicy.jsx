74120a3f0624183c001efbcef96533f4a609d5da
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-18 05:16:02 +0100

    create private policy jsx component

---
+++ src/Components/private-policy/Privatepolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.Privatepolicy.module.scss';
+
+function Privatepolicy() {
+  return (
+    <div>
+        Privatepolicy
+    </div>
+  );
+}
+
+export default Privatepolicy
