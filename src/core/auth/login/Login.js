a488dff6f88bca49ee05d3507698eba09710baca
Emmanuel Adewoye <emmaxfans@gmail.com>
2022-11-19 15:40:01 +0100

    feat(TEA-160): Adding new styling to the login-page

--- src/core/auth/login/Login.js
+++ src/core/auth/login/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../pages/Login.css'
+import LoginBottom from './LoginBottom';
+import LoginForm from './LoginForm';
 
-const Login = () => {
-  return <div>Login</div>;
+
+const Login = () =>{
+
+    return(
+
+        <div className="Login_page">
+            <LoginForm />
+            <LoginBottom/>   
+        </div>
+    )
 };
 
-export default Login;
+export default Login
