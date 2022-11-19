5747eb7be56509bf08aefdc509d3c05295dfe4b6
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-11-19 17:24:49 +0100

    Style(TEA-79): External page--FAQs
    Developed the FAQs page both mobile and desktop views
    
    please for the routing -- import FAQ from './pages/FAQ/FAQ';

--- src/pages/FAQ/static.js
+++ src/pages/FAQ/static.js
@@ -1,87 +1,86 @@
-export const subRoute = [
-    {
-      id: 0,
-      heading: "Account",
-      location: "/faq/account",
-    },
-    {
-      id: 1,
-      heading: "Tasks",
-      location: "/faq/tasks",
-    },
-    {
-      id: 2,
-      heading: "Premium",
-      location: "/faq/premium",
-    },
-  ];
-  
-  export const QandA_account = [
-    {
-      id: 0,
-      question: "How do I back up my data?",
-      answer: "You can connect to your google account.",
-    },
-    {
-      id: 1,
-      question: "How can I get a subscription plan?",
-      answer: "You can connect to your google account.",
-    },
-    {
-      id: 2,
-      question: "How can I change my password?",
-      answer: "You can connect to your google account.",
-    },
-  ];
-  
-  export const QandA_tasks = [
-    {
-      id: 0,
-      question: "How do I edit my list?",
-      answer: "By clicking on the edit button.",
-    },
-    {
-      id: 1,
-      question: "How do I use Ticked?",
-      answer: "You can connect to your google account.",
-    },
-    {
-      id: 2,
-      question: "How do I edit th status of new tasks?",
-      answer: "By clicking on the edit button.",
-    },
-    {
-      id: 3,
-      question: "How do I use the reminder feature?",
-      answer: "By clicking on the edit button.",
-    },
-    {
-      id: 4,
-      question: "How do I get notification?",
-      answer: "By clicking on the edit button.",
-    },
-  ];
-  
-  export const QandA_premium = [
-    {
-      id: 0,
-      question: "Can I get a refund?",
-      answer: "No, it’s non-refundable.",
-    },
-    {
-      id: 1,
-      question: "Howo can I get a subscription plan?",
-      answer: "No, it’s non-refundable.",
-    },
-    {
-      id: 2,
-      question: "What is Ticked premium?",
-      answer: "No, it’s non-refundable.",
-    },
-    {
-      id: 3,
-      question: "What is Ticked free?",
-      answer: "By clicking on the edit button.",
-    },
-  ];
-  
+export const QandA = [
+  {
+    id: 0,
+    question: "How do I back up my data?",
+    answer: "You can connect to your google account",
+  },
+  {
+    id: 1,
+    question: "Can i delete my account?",
+    answer: "Yes you can.",
+  },
+  {
+    id: 2,
+    question: "Will my data be stored on Ticked?",
+    answer: "Your data will be securely stored in the cloud.",
+  },
+  {
+    id: 4,
+    question: "How can i change my password?",
+    answer: "This can be done in the profile page.",
+  },
+  {
+    id: 5,
+    question: "How do i get notifications?",
+    answer: "Notifications will be sent out.",
+  },
+  {
+    id: 6,
+    question: "How do I edit my list?",
+    answer: "There is an edit button provided for this purpose.",
+  },
+  {
+    id: 7,
+    question: "How do I use Ticked?",
+    answer: "By clicking on the new task button.",
+  },
+  {
+    id: 8,
+    question: "How do I edit the status of tasks?",
+    answer: "By clicking on the edit button.",
+  },
+  {
+    id: 9,
+    question: "How do I add new tasks?",
+    answer: "By clicking on the new task button.",
+  },
+  {
+    id: 10,
+    question: "How do I use the reminder feature?",
+    answer: "You need to set a reminder then you will be notified at that said time.",
+  },
+  {
+    id: 11,
+    question: "Can I get  a refund?",
+    answer: "No, it’s non-refundable.",
+  },
+  {
+    id: 12,
+    question: "How can I get a subscription plan?",
+    answer: "There are options you can select from in the subscription page.",
+  },
+  {
+    id: 13,
+    question: "What is Ticked premium?",
+    answer: "Tricked premium guarantee continuous usage of Ticked.",
+  },
+  {
+    id: 14,
+    question: "Please more info on Ticked free?",
+    answer:
+      "Ticked free is a trial period which gives the user a feel of what to expect.",
+  },
+  {
+    id: 15,
+    question: "Is there gift card?",
+    answer: "There is none for now.",
+  },
+];
+
+export const contact = {
+  header: " Hi, How can we help you today?",
+  sub_text: "Below are answers to our Frequently Asked Questions.",
+  contact:
+    "Cannot find the answer you’re looking for? Please chat with our friendly team.",
+  link: "Contact Us",
+};
