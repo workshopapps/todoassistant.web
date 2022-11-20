cc1c9942ee4d739c6a94c364dabc1107b2c3053d
OLAGOKE TUNDE OLUJIDE <olagoketunde600@gmail.com>
2022-11-20 17:04:10 +0100

    feat:(TEA-106) Check Mail

--- src/core/auth/checkMail/CheckM.jsx
+++ src/core/auth/checkMail/CheckM.jsx
@@ -10,7 +10,7 @@ const CheckM = () => {
                     <img id='Sms'  src={Sms} alt="Footer_Logo" />
                     <h1>Check your mail</h1>
                     <p id='email'>We have sent a verification code to <br />annadavid@gmail.com</p>
-                    <button>Go to Email App  &nbsp; &#8594;</button>
+                    <a href="https://accounts.google.com"><button>Go to Email App  &nbsp; &#8594;</button></a>
                     <h6 id='code'>Resend verification code </h6>
                     <h6 id='help'>Contact help center </h6>
                     <p>Did you recieve the email? Check your spam filter <br /> or <em id='address'>try another email address</em></p>
