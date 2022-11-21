0410cf332da7ada17a701fe006318c0a82af5eff
James Ishaku <66888791+Jamesishaku@users.noreply.github.com>
2022-11-21 04:27:46 +0100

    feat(TEA-114): Added New Task page

--- src/core/todo/NewTask.jsx
+++ src/core/todo/NewTask.jsx
@@ -2,18 +2,19 @@
 import Form from "../../components/NewTask/Form";
 import Header from "../../components/NewTask/Header";
 import Sidebar from "../../components/NewTask/Sidebar";
-import "./NewTask.scss"
+import styles from "./NewTask.module.scss"
 
 function NewTask() {
   return (
-    <div className="App">
+    <div className={ styles.newTaskBody }>
+    <div className={styles.newTaskContainer}>
       <Sidebar />
-      <main>
+        <main className={ styles.mainContainer }>
         <Header />
         <Form />
       </main>
     </div>
-
+  </div>
   );
 }
 
