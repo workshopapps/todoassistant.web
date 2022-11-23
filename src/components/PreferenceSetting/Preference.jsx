7ae6254aeed5cfd4c94d689823393df3cfcce44f
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-23 12:06:49 +0100

    Fix the Preference setting page

--- src/components/PreferenceSetting/Preference.jsx
+++ src/components/PreferenceSetting/Preference.jsx
@@ -1,13 +1,18 @@
-import PrefSetHNN from "./PreferenceSettingHeadnNav";
+import Header from "../accountPreferences/Header";
+import Sidebar from "../NewTask/Sidebar";
 import PrefSetForm from "./PreferenceSettingForm";
+import style from "./Preference.module.css";
 
 function PreferenceSettingEdit() {
-	return (
-		<>
-			<PrefSetHNN />
-			<PrefSetForm />
-		</>
-	);
+  return (
+    <div className={style.psf}>
+      <Sidebar />
+      <div className={style.preference}>
+        <Header />
+        <PrefSetForm />
+      </div>
+    </div>
+  );
 }
 
 export default PreferenceSettingEdit;
