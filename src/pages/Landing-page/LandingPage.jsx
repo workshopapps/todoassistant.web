c3a5ff3afca41d668aada1e9cb7074e5d393f9c0
Kingsley Solomon <kinxly@gmail.com>
2022-11-23 11:52:07 +0100

    fix: route issues

--- src/pages/Landing-page/LandingPage.jsx
+++ src/pages/Landing-page/LandingPage.jsx
@@ -4,8 +4,11 @@ import Gift from "../../components/landing-page/gift/Gift";
 
 import HappyClient from "../../components/landing-page/happy-client/HappyClient";
 import Hero from "../../components/landing-page/hero-section/HeroSection";
+import Occupation from "../../components/landing-page/occupation/Occupation";
 import SectionThree from "../../components/landing-page/section-three/SectionThree";
 import SectionTwo from "../../components/landing-page/section-two/SectionTwo";
+import TodoListSection from "../../components/landing-page/todoList/TodoListSection";
+import VirtualAssistant from "../../components/landing-page/virtualAssistant/VirtualAssistant";
 import WhyTicked from "../../components/landing-page/why-ticked/WhyTicked";
 import Footer from "../../layout/footer/Footer";
 import Header from "../../layout/header/Header";
@@ -15,6 +18,9 @@ const LandingPage = () => {
     <>
       <Header />
       <Hero />
+      <Occupation />
+      <VirtualAssistant />
+      <TodoListSection />
       <SectionTwo />
       <SectionThree />
       <WhyTicked />
