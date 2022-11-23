aaa1aac1485071e1dac19b4f58552b30c4ecec44
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-23 09:25:36 +0100

    refactor: refactor couple pages

---
+++ docs/STYLE-GUIDE.md
@@ -0,0 +1,10 @@
+# General Styles
+
+## Branding
+
+Uploading your own project assets is super easy: just open the assets > pick the folder that relates more with the image and drop or replace.
+
+## Colors
+
+You can refer to the scss folder and check the \_variables.scss to check for color assigned to variables.
+![logo]('https://www.figma.com/file/3f7fQODyHQ0PC7e2skvD8O/To-do-design-system?node-id=761%3A68554&t=HkfPIWTpm2F2nKbz-4)
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import ErrorBoundary from "./layout/error-boundary/ErrorBoundary";
+// import ErrorBoundary from "./layout/error-boundary/ErrorBoundary";
 import AccountPreferences from "./components/accountPreferences/account/AccountPreferences";
 import ProPreferences from "./components/accountPreferences/pro/ProPreferences";
 import PreferenceSettingEdit from "./components/PreferenceSetting/Preference";
@@ -8,7 +8,7 @@ import Signup from "./core/auth/signup/Signup";
 import GeneralLoading from "./layout/general-loading/GeneralLoading";
 
 import FAQ from "./pages/FAQ/FAQ";
-import { About, Career, Contact, LandingPage, PrivatePolicy } from "./pages";
+import { Career, Contact, LandingPage, PrivatePolicy } from "./pages";
 import Dasboard from "./components/dasboard/Dasboard";
 import Home from "./components/userDasboardComponents/home/Home";
 import ChatBox from "./core/chatbox/ChatBox";
@@ -23,7 +23,7 @@ import Notification from "./components/notification/Notification";
 import Notifications from "./pages/Notifications/Notifications";
 import VaProfile from "./components/VA-Profile/VaProfile";
 import VaModal1 from "./components/VA-modal/VaModal1";
-import Login from "./components/Login";
+// import Login from "./components/Login/Login";
 import ErrorMain from "./components/404";
 import SettingsProfile from "./core/settings/profile/SettingsProfile";
 import Otp from "./core/auth/otp/Otp";
@@ -33,63 +33,62 @@ import NewPassword from "./components/resetPassword and newPassword pages/NewPas
 import ResetPassword from "./components/resetPassword and newPassword pages/ResetPassword";
 
 import VaDasboard from "./components/vaDashboard/VaDasboard";
+import LoginPage from "./core/auth/login/LoginPage";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<GeneralLoading text={`LOADING...`} />}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/career" element={<Career />} />
-          <Route path="/dashboard" element={<Dasboard />}>
-            <Route path="" element={<Home />} />
-            <Route path="assistant" element={<ChatBox />} />
-            <Route path="task" element={<Task />}></Route>
-            <Route path="detail" element={<Detail />} />
-            <Route path="sub" element={<Sub />} />
-            <Route path="clients" element={<Client />} />
-            <Route path="notification" element={<Notification />} />
-            <Route path="notifications" element={<Notifications />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="profile" element={<VaProfile />} />
-          </Route>
-          <Route path="/virtualassistance" element={<VaDasboard />}>
-            <Route path="" element={<Home />} />
-            <Route path="notifications" element={<Notifications />} />
-           
-          </Route>
-          <Route path="/about" element={<About />} />
+    // <ErrorBoundary>
+    <Suspense fallback={<GeneralLoading text={`LOADING...`} />}>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/dashboard" element={<Dasboard />}>
+          <Route path="" element={<Home />} />
+          <Route path="assistant" element={<ChatBox />} />
+          <Route path="task" element={<Task />}></Route>
+          <Route path="detail" element={<Detail />} />
+          <Route path="sub" element={<Sub />} />
+          <Route path="clients" element={<Client />} />
+          <Route path="notification" element={<Notification />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="profile" element={<VaProfile />} />
+        </Route>
+        <Route path="/virtualassistance" element={<VaDasboard />}>
+          <Route path="" element={<Home />} />
+          <Route path="notifications" element={<Notifications />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
 
-          <Route path="/policy" element={<PrivatePolicy />} />
-          <Route path="/account" element={<AccountPreferences />} />
-          <Route path="/pro" element={<ProPreferences />} />
-          <Route path="/account/edit" element={<PreferenceSettingEdit />} />
+        <Route path="/policy" element={<PrivatePolicy />} />
+        <Route path="/account" element={<AccountPreferences />} />
+        <Route path="/pro" element={<ProPreferences />} />
+        <Route path="/account/edit" element={<PreferenceSettingEdit />} />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/newpassword" element={<NewPassword />} />
-          <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
 
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/newtask" element={<NewTask />} />
+        <Route path="/va1" element={<VaModal1 />} />
+        <Route path="/CheckM" element={<CheckM />} />
+        <Route path="/otp" element={<Otp />} />
 
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/newtask" element={<NewTask />} />
-          <Route path="/va1" element={<VaModal1 />} />
-          <Route path="/CheckM" element={<CheckM />} />
-          <Route path="/otp" element={<Otp />} />
+        <Route path="/settings/profile" element={<SettingsProfile />} />
 
-          
-          <Route path="/settings/profile" element={<SettingsProfile />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
 
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/newpassword" element={<NewPassword/>} />
-          <Route path="/resetpassword" element={<ResetPassword />} />
-
-          {/* 404-error handler */}
-          {/* <Route path="*" element={<GeneralLoading text="PAGE NOT FOUND" />} /> */}
-          <Route path="*" element={<ErrorMain />} />
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+        {/* 404-error handler */}
+        {/* <Route path="*" element={<GeneralLoading text="PAGE NOT FOUND" />} /> */}
+        <Route path="*" element={<ErrorMain />} />
+      </Routes>
+    </Suspense>
+    // </ErrorBoundary>
   );
 }
 
