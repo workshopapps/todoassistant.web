1fd555067fe726841244cd634b619a534ca9b2c6
Kingsley Solomon <kinxly@gmail.com>
2022-11-24 20:52:06 +0100

    feat: changed how it works section

--- src/components-II/landing-page/section-three/SectionThree.jsx
+++ src/components-II/landing-page/section-three/SectionThree.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Animate } from "../../../hooks/animation/AnimateIn";
+import Button from "../../button/ButtonComponent";
 import SectionThreeList from "./section-three-list/SectionThreeList";
 import styles from "./SectionThree.module.scss";
 
@@ -14,25 +15,42 @@ const SectionThree = () => {
       <section className={styles.container}>
         <div className={styles.main__tick}>
           <header>
-            <h3 className={styles.section__three_title}>How It Works?</h3>
-            <p className={styles.section__three_subtitle}>Get onboard easily with TICKED.</p>
+            <h3 className={styles.section__three_title}>How It Works</h3>
+            <p className={styles.section__three_subtitle}>
+              It's as easy as…
+            </p>
           </header>
-          <SectionThreeList
-            icon="https://res.cloudinary.com/kingsleysolomon/image/upload/v1668670551/hng/todoAppVirtualAssistant/task-square_bmrtgd.svg"
-            title="Create a task"
-            desc="Fill in your task title, task description, set date, set reminder, get a Virtual Assistant."
-          />
+          <div className={styles.steps}>
+            <SectionThreeList
+              icon="https://res.cloudinary.com/kingsleysolomon/image/upload/v1668670551/hng/todoAppVirtualAssistant/task-square_bmrtgd.svg"
+              title="Get on your dashboard"
+              desc="Describe your task, set the date and time for your task deadline."
+            />
+          </div>
 
-          <SectionThreeList
-            icon="https://res.cloudinary.com/kingsleysolomon/image/upload/v1668670567/hng/todoAppVirtualAssistant/timer-pause_zksz2j.svg"
-            title="Get a reminder"
-            desc="Receive reminders and notifications when your task is due as well as when it is overdue."
-          />
-          <SectionThreeList
+          <div className={styles.steps}>
+            <SectionThreeList
+              icon="https://res.cloudinary.com/kingsleysolomon/image/upload/v1668670567/hng/todoAppVirtualAssistant/timer-pause_zksz2j.svg"
+              title="Personalized Virtual Assistant"
+              desc="The in-app chat feature available allows you to communicate and assign tasks to a personalized Assistant."
+            />
+          </div>
+          <div className={styles.steps}>
+            <Button
+              link={`/`}
+              style={{ width: `184px` }}
+              title={`get ticked`}
+            />
+            <p>
+              Receive notifications when you complete your tasks and a call when
+              deadlines are fast approaching.{" "}
+            </p>
+          </div>
+          {/* <SectionThreeList
             icon="https://res.cloudinary.com/kingsleysolomon/image/upload/v1668670583/hng/todoAppVirtualAssistant/tick-circle_cw0mdk.svg"
             title="Stay Proactive"
             desc="Stay one step ahead by scheduling and organising your tasks to ensure you don’t miss out on any deadline."
-          />
+          /> */}
         </div>
       </section>
       <div>
