c7c307e5f09da1b76f03260be5977e8b627c4e35
James Ishaku <66888791+Jamesishaku@users.noreply.github.com>
2022-11-25 23:51:39 +0100

    feat: (TEA-230) Format Task (Edit, delete, update status)
    1. Added task details page
    2. Ability to edit and delete a task

--- src/core/todo/TaskDetials.jsx
+++ src/core/todo/TaskDetials.jsx
@@ -5,19 +5,23 @@ import { HiOutlineMenu } from "react-icons/hi";
 import { Link, useNavigate } from "react-router-dom";
 import edit from "../../assets/edit.svg";
 import trash from "../../assets/trash.svg";
+import {TaskCtx} from "../../contexts/taskContext/TaskContextProvider"
+import { useContext } from "react";
 
-const data = {
-  title: "Go to the bank",
-  start_time: "11:00am",
-  end_time: "12:00am",
-  repeat: "Never Repeat",
-  description: "I need to upgrade my account and request for a new token",
-  va: "Get a call from an assistant to remind you"
-};
+// const data = {
+//   title: "Go to the bank",
+//   start_time: "11:00am",
+//   end_time: "12:00am",
+//   repeat: "Never Repeat",
+//   description: "I need to upgrade my account and request for a new token",
+//   va: "Get a call from an assistant to remind you"
+// };
 
 const TaskDetails = () => {
   const [drop, setDrop] = useState(0);
   const navigate = useNavigate();
+
+  const {task} = useContext(TaskCtx)
   return (
     <div className={styles.TaskDetialsContainer}>
       <div className={styles.TaskDetialsSidebar}>
@@ -37,26 +41,26 @@ const TaskDetails = () => {
           </div>
         </div>
         <div className={styles.TaskDetailsBody}>
-          <h1 className={styles.TaskDetailsBodyTitle}>{data.title}</h1>
+          <h1 className={styles.TaskDetailsBodyTitle}>{task.title}</h1>
           <div className={styles.TaskDetailsBodyItem1}>
             <h2>Start Time</h2>
-            <p>{data.start_time}</p>
+            <p>{task.start_time}</p>
           </div>
           <div className={styles.TaskDetailsBodyItem2}>
             <h2>End Time</h2>
-            <p>{data.end_time}</p>
+            <p>{task.end_time}</p>
           </div>
           <div className={styles.TaskDetailsBodyItem3}>
             <h2>Repeat</h2>
-            <p>{data.repeat}</p>
+            <p>{task.repeat}</p>
           </div>
           <div className={styles.TaskDetailsBodyItem4}>
             <h2>Description</h2>
-            <p>{data.description}</p>
+            <p>{task.description}</p>
           </div>
           <div className={styles.TaskDetailsBodyItem5}>
             <h2>Virtual Assistant</h2>
-            <p>{data.va}</p>
+            <p>{task.va}</p>
           </div>
           <div
             onClick={() => {
@@ -73,7 +77,15 @@ const TaskDetails = () => {
                 <img src={edit} alt="edit" />
                 <p>Edit task</p>
               </Link>
-              <div onClick={() => navigate("/dashboard")}>
+              <div onClick={() => {
+                 delete task.title;
+                delete task.start_time;
+                delete task.end_time;
+                delete task.repeat;
+                delete task.description;
+                delete task.va;
+                navigate("/dashboard")
+              }}>
                 <img src={trash} alt="trash" />
                 <p>Delete task</p>
               </div>
