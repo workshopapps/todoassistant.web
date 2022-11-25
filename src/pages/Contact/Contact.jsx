b5d6e1ca59c908ef30d1a64f75d158aac2fa94e1
Kingsley Solomon <kinxly@gmail.com>
2022-11-25 23:12:43 +0100

    refactor: contact page and faq page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,10 +6,12 @@ import Map from "../../components-II/contact/contact-map/Map";
 // import People from "../../assets/people.png";
 import Talk from "../../components-II/contact/talk/Talk";
 import Layout from "../../layout/Layout";
+import ContactHero from "../../components-II/contact/contact-hero/ContactHero";
 
 const Contact = () => {
   return (
     <Layout>
+      <ContactHero />
       <ContactForm />
       <Talk />
       <Map />
