f62602075c9b092a00f3939c6a96b0374e8a6efc
Oladunni Waters <waters.oladunni@gmail.com>
2022-11-30 15:18:27 +0100

    feat(FRO-43):Footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 import { Link, NavLink } from "react-router-dom";
-import facebook from "../../assets/facebook.png";
-import twitter from "../../assets/twitter.png";
-import instagram from "../../assets/instagram.png";
+//import facebook from "../../assets/facebook.png";
+//import twitter from "../../assets/twitter.png";
+//import instagram from "../../assets/instagram.png";
 import NewsLetter from "./news-letter-form/NewsLetter";
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
@@ -17,33 +19,42 @@ const Footer = () => {
       </NavLink>
       <section className={styles.footer_text}>
         <NewsLetter />
-        <p>
+        <div className={styles.p_container}>
+        <p className={styles.p_tag}>
           Subscribe to our monthly newsletter and be among the first to be
           notified on an update of the App. Our newsletter is full of important
           announcements and new feature releases, tips and how-tos, and the
-          occasional peek into life at TICKED. We will treat your email address
+          occasional peek into life at TICKED.
+        </p>
+        <p className={styles.p_tag}>  
+          We will treat your email address
           exactly how we would want ours treated: we will only use it for this
           list, and if you choose to unsubscribe, we will forget we ever had it.
         </p>
+        </div>
       </section>
       <div className={styles.footer_links}>
         <Link to="/about">About Us</Link>
         <Link to="/contact">Contact Us</Link>
-        <Link to="/faq">FAQs</Link>
-        <Link to="/career">Career</Link>
+        <Link to="/subscribe">Subscription Plan</Link>
         <Link to="/policy">Privacy</Link>
       </div>
       <div className={styles.footer_socials}>
         <NavLink to={null}>
-          <img src={facebook} alt="logo" />
+          <FaFacebookF className={styles.footer_icons}/>
         </NavLink>
         <NavLink to={null}>
-          <img src={twitter} alt="logo" />
+          <FaTwitter className={styles.footer_icons}/>
         </NavLink>
         <NavLink to={null}>
-          <img src={instagram} alt="logo" />
+          <FaInstagram className={styles.footer_icons}/>
         </NavLink>
       </div>
+    
+       <div className={styles.footer_copywrite}>
+              (c) Ticked App 2022 All Rights Reserved | HNGi9 Product
+      </div>
+      
     </footer>
   );
 };
