87b2d0260d50816f958947b8be3aa88c7af61350
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-12-02 16:47:46 +0100

    feat: New UI with functionalities

--- src/components/userDasboardComponents/home/Home.js
+++ src/components/userDasboardComponents/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; // ,{ useContext, useState }
+import React, { useState, useContext, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.scss";
 import addIcon from "../../../assets/newAdd.png";
@@ -8,8 +8,8 @@ import addIcon from "../../../assets/newAdd.png";
 // import downarrowIcon from "../../../assets/arrow-down.svg";
 // import TodoCard from "./TodoCard";
 // import Log from "./Log";
-// import useTasksLoading from "../../../hooks/tasks/useTasksLoading";
-// import { TaskCtx } from "../../../contexts/taskContext/TaskContextProvider";
+import useTasksLoading from "../../../hooks/tasks/useTasksLoading";
+import { TaskCtx } from "../../../contexts/taskContext/TaskContextProvider";
 import DashboardTabs from "../../dasboard/DashboardTabs";
 import TaskItems from "../../dasboard/TaskItems";
 import CreateTask from "../../createTask/CreateTask";
@@ -22,8 +22,14 @@ export default function Home() {
   // }
   // const tasks = JSON.parse(localStorage.getItem("myTasks"));
   // const navigate = useNavigate();
-  // const { getClickedTask } = useContext(TaskCtx);
-  // useTasksLoading();
+  useTasksLoading();
+  const { tasks } = useContext(TaskCtx);
+  // const [mytasks, setMytasks] = useState(tasks);
+  useEffect(() => {
+    // setMytasks(tasks);
+  }, [tasks]);
+
+  // console.log(mytasks);
   // const dummy_todos = [
   //   { va: true, completed: false, todoName: "Resolve frontend bugs", id: "1" },
   //   { va: true, completed: true, todoName: "Learn NextJS", id: "2" },
@@ -84,7 +90,7 @@ export default function Home() {
         <div className={styles.left}>
           <h1>Welcome to Ticked</h1>
           <div className={styles.todoTabs}>
-            <DashboardTabs tabNames={["All Task", "Mine", "Assistant"]} />
+            <DashboardTabs tabNames={["All Task"]} />
           </div>
         </div>
         <div
@@ -100,9 +106,9 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.allTasks}>
-        <TaskItems status="PENDING" />
-        <TaskItems status="DONE" />
-        <TaskItems status="OVERDUE" />
+        <TaskItems status="PENDING" myTasks={tasks} />
+        <TaskItems status="DONE" myTasks={tasks} />
+        <TaskItems status="EXPIRED" myTasks={tasks} />
       </div>
     </div>
     // <div className={styles.wrapper}>
