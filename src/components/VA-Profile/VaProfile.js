f01dc9cb96743f5c8901ba6d0508bde7d99cd16c
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-12-02 01:28:47 +0100

    Feat/FRO-65

--- src/components/VA-Profile/VaProfile.js
+++ src/components/VA-Profile/VaProfile.js
@@ -1,14 +1,20 @@
 import { CgProfile } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { BsStarHalf, BsStar, BsStarFill } from "react-icons/bs";
 import mainProfileImage from "../../assets/Ellipse 2.svg";
 import style from "./VaProfile.module.scss";
 
 function VAProfile() {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
+    <div className={style.app}>
       <div className={style.major_container}>
-        <div className={style.back_button_container}>
+        <div
+          onClick={() => navigate(-1)}
+          className={style.back_button_container}
+        >
           <AiOutlineArrowLeft className={style.AiOutlineArrowLeft} />
           <span className={style.back_button_header}>Back</span>
         </div>
@@ -23,82 +29,57 @@ function VAProfile() {
           </div>
         </div>
 
-        <div className={style.profile_container}>
-          <img
-            src={mainProfileImage}
-            alt=""
-            className={style.main_profile_image}
-          />
+        <div className={style.profile_raw_box}>
+          <div className={style.profile_container}>
+            <img
+              src={mainProfileImage}
+              alt=""
+              className={style.main_profile_image}
+            />
 
-          <div className={style.profile_container_items}>
-            <div className={style.profile_container_details1}>
-              <p className={style.profile_container_items_header}>
-                Sandra Davids
-              </p>
-              <p className={style.profile_container_items_id}>ID: 204191402</p>
-            </div>
-
-            <div className={style.profile_container_details2}>
-              <p className={style.profile_container_items_paragraph1}>Role</p>
-              <p className={style.profile_container__virtualA}>
-                Virtual Assistant
-              </p>
-            </div>
-
-            <div className={style.profile_container_details3}>
-              <p className={style.profile_container_items_paragraph2}>Bio</p>
-              <p className={style.profile_container_bio}>
-                I am available 24 hours a day. You just sit back and count on
-                me.
-              </p>
-            </div>
-
-            <div className={style.profile_container_details4}>
-              <div>
-                <p className={style.profile_container_items_paragraph3}>Phone</p>
-                <p className={style.profile_container_num}>0802 290 4580</p>
-              </div>
-
-              <div>
-                <p className={style.profile_container_items_paragraph4}>
-                  Email Address
-                </p>
+            <div className={style.profile_container_items}>
+              <div className={style.profile_container_details1}>
                 <p className={style.profile_container_items_header}>
-                  sandradavid@ticked.com
+                  Sandra Davids
                 </p>
-              </div>
-            </div>
-
-            <div className={style.profile_container_details5}>
-              <div>
-                <p className={style.profile_container_items_paragraph5}>
-                  Address
-                </p>
-                <p className={style.profile_container_address}>
-                  25 Adebanjo Street
+                <p className={style.profile_container_items_id}>
+                  ID: 204191402
                 </p>
               </div>
 
-              <div>
-                <p className={style.profile_container_items_city}>City</p>
-                <p className={style.profile_container_items_header}>Lagos</p>
+              <div className={style.profile_container_details3}>
+                <p className={style.profile_container_items_paragraph1}>Bio</p>
+                <p className={style.profile_container_bio}></p>
               </div>
 
-              <div>
-                <p className={style.profile_container_items_paragraph6}>
-                  Country
+              <div className={style.profile_container_details4}>
+                <div>
+                  <p className={style.profile_container_items_paragraph1}>
+                    Phone
+                  </p>
+                  <p className={style.profile_container_num}>0802 290 4580</p>
+                </div>
+
+                <div>
+                  <p className={style.profile_container_items_paragraph1}>
+                    Email Address
+                  </p>
+                  <p className={style.profile_container_items_header}>
+                    sandradavid@ticked.com
+                  </p>
+                </div>
+              </div>
+
+              <div className={style.profile_container_details}>
+                <p className={style.profile_container_items_paragraph1}>
+                  Skills
                 </p>
-                <p className={style.profile_container_items_header}>Nigeria</p>
-              </div>
-            </div>
-
-            <div className={style.profile_container_details}>
-              <p className={style.profile_container_items_paragraph7}>Skills</p>
-              <div className={style.button_container}>
-                <button className={style.button1}>Excel Software</button>
-                <button className={style.button2}>Time Management</button>
-                <button className={style.button3}>Communication</button>
-                <button className={style.button4}>Writing Skills</button>
+                <div className={style.button_container}>
+                  <button className={style.button1}>Excel Software</button>
+                  <button className={style.button1}>Time Management</button>
+                  <button className={style.button1}>Communication</button>
+                  <button className={style.button1}>Writing Skills</button>
+                </div>
               </div>
             </div>
           </div>
@@ -132,13 +113,6 @@ function VAProfile() {
                   <p className={style.profile_stats_items_header2}>5</p>
                   <p className={style.profile_stats_items_paragraph2}>
                     Assigned Client(s)
-                  </p>
-                </div>
-
-                <div className={style.profile_stats_items}>
-                  <p className={style.profile_stats_items_header3}>1</p>
-                  <p className={style.profile_stats_items_paragraph3}>
-                    Unassigned Client(s)
                   </p>
                 </div>
               </div>
