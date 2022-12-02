374c22543865a748ab78f0bd221e646577e14870
PeroHub <65111015+PeroHub@users.noreply.github.com>
2022-12-02 08:16:17 -0800

    pull

--- src/layout/footer/news-letter-form/NewsLetter.jsx
+++ src/layout/footer/news-letter-form/NewsLetter.jsx
@@ -79,7 +79,7 @@ const NewsLetter = () => {
           Email Address
         </label>
         <div className={style.input_container}>
-           <input className={style.input} style={{outline: "none"}} autoComplete onChange={(e) => setEmail(e.target.value)} type="email" value={email} placeholder="Enter email" />
+           <input className={`style.input hover`} style={{outline: "none"}} autoComplete onChange={(e) => setEmail(e.target.value)} type="email" value={email} placeholder="Enter email" />
            <input onClick={handleNewsLetter} className={style.subscribe_btn} style={{cursor: "pointer"}} type="button" value="Subscribe"/>
         </div>
       </form>
