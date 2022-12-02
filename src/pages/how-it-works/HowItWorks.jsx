44b238fe861a2d9e702c8c19b2ee69061bf104b8
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-12-02 21:25:21 +0100

    mm

--- src/pages/how-it-works/HowItWorks.jsx
+++ src/pages/how-it-works/HowItWorks.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Download from "../../components/landing-page/download/Download";
-import HowSections  from "../../components/NewHowItWorks/HowSections";
+import HowSections from "../../components/NewHowItWorks/HowSections";
 import Layout from "../../layout/Layout";
 
 const HowItWorks = () => {
