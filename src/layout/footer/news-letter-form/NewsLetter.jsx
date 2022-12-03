ce91d2e90422e135096742049d88082dfc0eb538
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-12-03 08:06:13 +0100

    style(FRO-40): added hover effect on all external pages button

--- src/layout/footer/news-letter-form/NewsLetter.jsx
+++ src/layout/footer/news-letter-form/NewsLetter.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import style from "./newsLetter.module.scss";
-import axios from "axios"
+import axios from "axios";
 
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
 // import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import Slide from "@mui/material/Slide";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export function AlertDialogSlide({open,handleClose, error }) {
- 
-
+export function AlertDialogSlide({ open, handleClose, error }) {
   return (
     <div>
       <Dialog
@@ -27,11 +25,44 @@ export function AlertDialogSlide({open,handleClose, error }) {
         onClose={handleClose}
       >
         <DialogContent>
-          {error ? <p>En error occured, please try again letter</p> :   <Box sx={{display: "flex", gap: "10px", justifyContent: "center", alignItems: "center"}}>
-            <svg style={{height: "60px"}} aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><circle  stroke="currentColor" cx="27" cy="27" fill="none" r="25"></circle><path  stroke="currentColor" d="M14.1 27.2l7.1 7.2 16.7-16.8" fill="none"></path></svg>
-            <Typography>Thanks, your subscription has been confirmed. You've been added to our list and will hear from us soon.</Typography>
-          </Box>}
-        
+          {error ? (
+            <p>En error occured, please try again letter</p>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                gap: "10px",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <svg
+                style={{ height: "60px" }}
+                aria-hidden="true"
+                focusable="false"
+                fill="currentColor"
+                viewBox="0 0 54 54"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  stroke="currentColor"
+                  cx="27"
+                  cy="27"
+                  fill="none"
+                  r="25"
+                ></circle>
+                <path
+                  stroke="currentColor"
+                  d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                  fill="none"
+                ></path>
+              </svg>
+              <Typography>
+                Thanks, your subscription has been confirmed. You've been added
+                to our list and will hear from us soon.
+              </Typography>
+            </Box>
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>OK</Button>
@@ -42,10 +73,10 @@ export function AlertDialogSlide({open,handleClose, error }) {
 }
 
 const NewsLetter = () => {
-  const [ email, setEmail ] = useState("")
-  const [ error, setError ] = useState(false)
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
-  console.log(error)
+  console.log(error);
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -54,10 +85,10 @@ const NewsLetter = () => {
 
   const handleClose = () => {
     setOpen(false);
-    setEmail("")
+    setEmail("");
   };
 
-  const validateEmail = (email) => {
+  const validateEmail = email => {
     return String(email)
       .toLowerCase()
       .match(
@@ -65,39 +96,51 @@ const NewsLetter = () => {
       );
   };
 
-// console.log("changes")
+  // console.log("changes")
   const handleNewsLetter = async () => {
-    if(email !== "" && validateEmail(email)) {
+    if (email !== "" && validateEmail(email)) {
       try {
-        const response = await axios.post(
-          "/subscribe", 
-          {"email":  `"${email}"`},
-          )
-          if (response.status == 200 ) {
-            handleClickOpen()
-          }
-       } catch (error) {
-        console.error(error)
-        setError(true)
-        handleClickOpen()
-       }
+        const response = await axios.post("/subscribe", {
+          email: `"${email}"`
+        });
+        if (response.status == 200) {
+          handleClickOpen();
+        }
+      } catch (error) {
+        console.error(error);
+        setError(true);
+        handleClickOpen();
+      }
     }
-  }
+  };
   return (
     <>
-    <AlertDialogSlide handleClose={handleClose} open={open} error={error} />
-    <section className={style.news__letter}>
-      <h3 className={style.header}>Newsletter</h3>
-      <form className={style.form}>
-        <label className={style.label} htmlFor="email">
-          Email Address
-        </label>
-        <div className={style.input_container}>
-           <input className={`${style.input}`} style={{outline: "none"}} autoComplete onChange={(e) => setEmail(e.target.value)} type="email" value={email} placeholder="Enter email" />
-           <input onClick={handleNewsLetter} className={style.subscribe_button} type="button" value="Subscribe"/>
-        </div>
-      </form>
-    </section>
+      <AlertDialogSlide handleClose={handleClose} open={open} error={error} />
+      <section className={style.news__letter}>
+        <h3 className={style.header}>Newsletter</h3>
+        <form className={style.form}>
+          <label className={style.label} htmlFor="email">
+            Email Address
+          </label>
+          <div className={style.input_container}>
+            <input
+              className={`${style.input}`}
+              style={{ outline: "none" }}
+              autoComplete
+              onChange={e => setEmail(e.target.value)}
+              type="email"
+              value={email}
+              placeholder="Enter email"
+            />
+            <input
+              onClick={handleNewsLetter}
+              className={`${style.subscribe_button} hover`}
+              type="button"
+              value="Subscribe"
+            />
+          </div>
+        </form>
+      </section>
     </>
   );
 };
