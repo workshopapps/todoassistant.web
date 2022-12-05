39f996e6417127222139415b70cd39aaecd92098
PeroHub <65111015+PeroHub@users.noreply.github.com>
2022-12-05 03:04:55 -0800

    changes made

--- src/layout/footer/news-letter-form/NewsLetter.jsx
+++ src/layout/footer/news-letter-form/NewsLetter.jsx
@@ -15,7 +15,28 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
+ <Box sx={{display: "flex", gap: "10px", justifyContent: "center", alignItems: "center"}}>
+    <svg style={{height: "60px"}} aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><circle  stroke="currentColor" cx="27" cy="27" fill="none" r="25"></circle><path  stroke="currentColor" d="M14.1 27.2l7.1 7.2 16.7-16.8" fill="none"></path></svg>
+    <Typography>Thanks, your subscription has been confirmed. You've been added to our list and will hear from us soon.</Typography>
+  </Box>
+
 export function AlertDialogSlide({open,handleClose, error }) {
+
+  let content;
+  if (error !== "error") {
+    if( error === "new") {
+      content = <Box sx={{display: "flex", gap: "10px", justifyContent: "center", alignItems: "center"}}>
+      <svg style={{height: "60px"}} aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><circle  stroke="currentColor" cx="27" cy="27" fill="none" r="25"></circle><path  stroke="currentColor" d="M14.1 27.2l7.1 7.2 16.7-16.8" fill="none"></path></svg>
+      <Typography>Thanks, your subscription has been confirmed. You've been added to our list and will hear from us soon.</Typography>
+    </Box>
+    }else if (error === "exist") {
+      content = <Typography>Email already Exist</Typography>
+    }
+  
+  }else {
+    content = <Typography>En error occured, please try again letter</Typography>
+
+  }
  
 
   return (
@@ -27,10 +48,7 @@ export function AlertDialogSlide({open,handleClose, error }) {
         onClose={handleClose}
       >
         <DialogContent>
-          {error ? <p>En error occured, please try again letter</p> :   <Box sx={{display: "flex", gap: "10px", justifyContent: "center", alignItems: "center"}}>
-            <svg style={{height: "60px"}} aria-hidden="true" focusable="false" fill="currentColor" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><circle  stroke="currentColor" cx="27" cy="27" fill="none" r="25"></circle><path  stroke="currentColor" d="M14.1 27.2l7.1 7.2 16.7-16.8" fill="none"></path></svg>
-            <Typography>Thanks, your subscription has been confirmed. You've been added to our list and will hear from us soon.</Typography>
-          </Box>}
+          {content}
         
         </DialogContent>
         <DialogActions>
@@ -43,7 +61,7 @@ export function AlertDialogSlide({open,handleClose, error }) {
 
 const NewsLetter = () => {
   const [ email, setEmail ] = useState("")
-  const [ error, setError ] = useState(false)
+  const [ error, setError ] = useState("")
 
   console.log(error)
   const [open, setOpen] = useState(false);
@@ -75,11 +93,24 @@ const NewsLetter = () => {
           )
           if (response.status == 200 ) {
             handleClickOpen()
+            setError("new")
+          }if (response.status == 400 ) {
+            setError("exist")
           }
        } catch (error) {
+
         console.error(error)
-        setError(true)
+        if (error.response.status == 400 ) {
+          setError("exist")
         handleClickOpen()
+
+        }else if (error.response.status == 500) {
+
+          setError("error")
+        handleClickOpen()
+
+        }
+        // handleClickOpen()
        }
     }
   }
