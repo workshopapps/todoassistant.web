50dbbe3999bc5dd76b6bf07446219a21d245dd54
Kingsley Solomon <kinxly@gmail.com>
2022-12-06 20:50:04 +0100

    fix: signup form

--- src/core/auth/signup/SignupForm.jsx
+++ src/core/auth/signup/SignupForm.jsx
@@ -1,7 +1,9 @@
-// import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { GoogleLogin } from "react-google-login";
 import google from "../../../assets/google.png";
+import MuiPhoneNumber from "material-ui-phone-number";
+import { useForm, Controller } from "react-hook-form";
+import "./Signup.scss";
 // import { AuthContext } from "../../../contexts/authContext/AuthContext";
 // import { gapi } from "gapi-script";
 // import { useForm } from "react-hook-form";
@@ -20,97 +22,55 @@ import {
   Stack,
   Typography
 } from "@mui/material";
-// import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+// import { useNavigate } from "react-router-dom";
+
+const clientId =
+  "407472887868-9a6lr7idrip6h8cgthsgekl84mo7358q.apps.googleusercontent.com";
+// const baseurl = "https://api.ticked.hng.tech/api/v1";
 
 const SignupForm = () => {
-  // const clientId =
-  //   "407472887868-9a6lr7idrip6h8cgthsgekl84mo7358q.apps.googleusercontent.com";
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   trigger,
-  //   formState: { errors }
-  // } = useForm();
-  // const baseurl = "https://api.ticked.hng.tech/api/v1";
-  // const [isChecked, setIsChecked] = useState(false);
-  // const [active, setActive] = useState(true);
-  // const [passwordShown, setPasswordShown] = useState(false);
-  // const [phone, setPhone] = useState();
-  // const { dispatch } = useContext(AuthContext);
-  // const navigate = useNavigate();
+  // states
+  const { handleSubmit, control } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      gender: "",
+      password: ""
+    }
+  });
+  const onSubmit = data => console.log(data);
 
-  // useEffect(() => {
-  //   const initClient = () => {
-  //     gapi.client.init({
-  //       clientId: clientId,
-  //       scope: ""
-  //     });
-  //   };
-  //   gapi.load("client:auth2", initClient);
-  // });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  // const onSuccess = res => {
-  //   googleSignUp(res?.profileObj);
-  // };
-  // const onFailure = err => {
-  //   console.log("failed:", err);
-  // };
+  // functions to handle states
+  const handleClickShowPassword = () => {
+    setShowPassword(prevState => {
+      return (prevState = !prevState);
+    });
+  };
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(prevState => {
+      return (prevState = !prevState);
+    });
+  };
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
-  // const handleOnChange = () => {
-  //   setIsChecked(!isChecked);
-  // };
-
-  // const googleSignUp = async body => {
-  //   try {
-  //     const response = await axios.post(`${baseurl}/googlelogin`, body);
-  //     if (response.status == 200 && response.data) {
-  //       localStorage.setItem(
-  //         "token",
-  //         JSON.stringify(response.data.access_token)
-  //       );
-  //       localStorage.setItem("user", JSON.stringify(response?.data));
-  //       navigate("/dashboard", { replace: true });
-  //       navigate(0);
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const HandleSubmit = async data => {
-  //   const { first_name, last_name, email, password, gender, date_of_birth } =
-  //     data;
-
-  //   try {
-  //     const response = await axios.post(
-  //       "https://api.ticked.hng.tech/api/v1/user",
-  //       {
-  //         first_name,
-  //         last_name,
-  //         email,
-  //         phone,
-  //         password,
-  //         gender,
-  //         date_of_birth
-  //       }
-  //     );
-  //     console.log(response);
-  //     login({ email, password }, dispatch);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // const togglePassword = () => {
-  //   setPasswordShown(!passwordShown);
-  // };
-
+  // display
   return (
     <Container
       sx={{
         padding: { xs: `1.5rem`, md: `1.5rem 3rem`, lg: `1.5rem 7rem` },
-        marginTop: { md: `3rem` },
-        maxWidth: `40rem !important`
+        marginTop: { md: `1rem` },
+        maxWidth: `40rem !important`,
+        height: `calc(100vh - 103px)`,
+        overflow: `auto`
       }}
     >
       <Box m={`2rem 0`}>
@@ -121,49 +81,145 @@ const SignupForm = () => {
           fontWeight={700}
           varient="h2"
         >
-          Welcome Back
+          Create Account
         </Typography>
       </Box>
 
       {/* Form fields */}
       <Stack
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         gap={3}
         component="form"
         noValidate
         autoComplete="off"
       >
+        {/* First Name */}
+        <Stack>
+          <InputLabel htmlFor="firstname">First Name</InputLabel>
+          <Controller
+            name="firstname"
+            control={control}
+            render={({ field }) => (
+              <OutlinedInput
+                sx={{ borderRadius: `8px` }}
+                id="firstname"
+                {...field}
+              />
+            )}
+          />
+        </Stack>
+        {/* last Name */}
+        <Stack>
+          <InputLabel htmlFor="lastname">Last Name</InputLabel>
+          <Controller
+            name="lastname"
+            control={control}
+            render={({ field }) => (
+              <OutlinedInput
+                required
+                sx={{ borderRadius: `8px` }}
+                id="lastname"
+                {...field}
+              />
+            )}
+          />
+        </Stack>
+
         {/* email address */}
         <Stack>
           <InputLabel htmlFor="email-address">Email Address</InputLabel>
-          <OutlinedInput
-            required
-            sx={{ borderRadius: `8px` }}
-            id="email-address"
-            type="email"
-            // value={formData.email}
-            // onChange={handleChange("email")}
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <OutlinedInput
+                required
+                sx={{ borderRadius: `8px` }}
+                id="email-address"
+                type="email"
+                {...field}
+              />
+            )}
+          />
+        </Stack>
+        <Stack>
+          <InputLabel htmlFor="phone">Phone Number</InputLabel>
+          <Controller
+            name={"phone"}
+            control={control}
+            render={({ field }) => (
+              <MuiPhoneNumber
+                variant="outlined"
+                defaultCountry={"ng"}
+                name="phone"
+                {...field}
+              />
+            )}
+          />
+        </Stack>
+        {/* Gender */}
+        <Stack>
+          <InputLabel htmlFor="gender">Gender</InputLabel>
+          <Controller
+            name="gender"
+            control={control}
+            render={({ field }) => (
+              <OutlinedInput
+                required
+                sx={{ borderRadius: `8px` }}
+                id="gender"
+                type="text"
+                placeholder="Enter male or female"
+                {...field}
+              />
+            )}
           />
         </Stack>
         {/* password */}
         <Stack>
           <InputLabel htmlFor="password">Password</InputLabel>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <OutlinedInput
+                {...field}
+                sx={{ borderRadius: `8px` }}
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            )}
+          />
+        </Stack>
+        {/* confirm password */}
+        <Stack>
+          <InputLabel htmlFor="password">Confirm Password</InputLabel>
           <OutlinedInput
             required
             sx={{ borderRadius: `8px` }}
             id="outlined-adornment-password"
-            // type={showPassword ? "text" : "password"}
-            // value={formData.password}
-            // onChange={handleChange("password")}
+            type={showConfirmPassword ? "text" : "password"}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
                   aria-label="toggle password visibility"
-                  // onClick={handleClickShowPassword}
-                  // onMouseDown={handleMouseDownPassword}
+                  onClick={handleClickShowConfirmPassword}
+                  onMouseDown={handleMouseDownPassword}
                   edge="end"
                 >
-                  {/* {showPassword ? <VisibilityOff /> : <Visibility />} */}
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               </InputAdornment>
             }
@@ -172,32 +228,31 @@ const SignupForm = () => {
         {/* forgot password text */}
         <Stack
           direction={`row`}
-          // direction={{ xs: `column`, sm: `row` }}
           alignItems={`center`}
           justifyContent={`space-between`}
         >
-          <FormControlLabel
-            control={
-              <Checkbox
-                sx={{
-                  fontSize: `12px`,
-                  color: `#714DD9`,
-                  "&.Mui-checked": {
-                    color: `#714DD9`
-                  }
-                }}
+          <Controller
+            name="checkbox"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...field}
+                    sx={{
+                      fontSize: `12px`,
+                      color: `#714DD9`,
+                      "&.Mui-checked": {
+                        color: `#714DD9`
+                      }
+                    }}
+                  />
+                }
+                label="By signing up, you agree to the Terms of service and Privacy Policy"
               />
-            }
-            label="Remember me"
+            )}
           />
-          <Link to="/resetpassword">
-            <Typography
-              fontSize={{ xs: `12px`, sm: `initial` }}
-              color={`#714DD9`}
-            >
-              Forgot Password ?
-            </Typography>
-          </Link>
         </Stack>
         {/* call to action btn */}
         <Stack>
@@ -215,29 +270,8 @@ const SignupForm = () => {
               }
             }}
           >
-            Sign in
+            Sign up
           </Button>
-          <Stack
-            margin={`1rem 0`}
-            direction={`row`}
-            // direction={{ xs: `column`, sm: `row` }}
-            alignItems={`center`}
-            justifyContent={`center`}
-            gap={1}
-          >
-            <Typography fontSize={{ xs: `12px`, sm: `initial` }}>
-              Don't have an account ?{" "}
-            </Typography>
-            <Link to="/signup">
-              <Typography
-                fontSize={{ xs: `12px`, sm: `initial` }}
-                color="#714DD9"
-                fontWeight={700}
-              >
-                Create Account
-              </Typography>
-            </Link>
-          </Stack>
           {/* continue with */}
           <Stack
             direction={`row`}
@@ -247,7 +281,7 @@ const SignupForm = () => {
           >
             <IconButton sx={{ width: `fit-content` }} color="secondary">
               <GoogleLogin
-                // clientId={clientId}
+                clientId={clientId}
                 render={renderProps => (
                   <img onClick={renderProps.onClick} src={google} alt="" />
                 )}
